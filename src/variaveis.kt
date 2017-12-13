2f4c8379f003bdaf4bd23b19df7e6141b72d2bcb
Hecton Domingos <hectonpdomingos@gmail.com>
2017-12-13 00:55:38 -0200

    conceito variaveis

---
+++ src/variaveis.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+
+    /*
+    Hecton P.Domingos 13/12/17
+
+
+     */
+
+    //Variável nome de tipo String que recebe o valor Dimitri
+    var nome:String = "Dimitri"
+    //Variável nome de tipo String que recebe o valor Domingos
+    var sobrenome:String = "Domingos"
+    //Variável idade de tipo Inteiro que recebe o valor 2
+    var idade:Int = 2
+    //Variável numeroPreferido de tipo Int que recebe o valor 9
+    var numeroPreferido: Int = 9
+
+    //Mostrando na tela uma frase com os valores
+    println("O meu nome é $nome e meu sobrenome é $sobrenome " +
+            "eu tenho $idade idade e o meu número preferido é $numeroPreferido")
+
+
+}
+
